b447f881bf962fe5881932dc84aabc10380abf96
Yordanka AGP <yordankaandre@gmail.com>
2019-12-09 00:23:03 +0700

    tambahin navbar di layout.blade.php

--- storage/framework/views/c09039140ec16af860811ff3b0db62f87aa3330e.php
+++ storage/framework/views/c09039140ec16af860811ff3b0db62f87aa3330e.php
@@ -18,15 +18,34 @@
 
     <!-- Styles -->
     <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
+    <style>
+        .separator{
+            width:1.5px;
+            height:25px;
+            background-color:orangered;
+        }
+
+        #time{
+            margin: 0 50px 0 15px;
+            font-size: 11px;
+            cursor:default;
+        }
+
+        .ask{
+            font-size: 14px;
+        }
+    </style>
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="<?php echo e(url('/')); ?>">
-                    <?php echo e(config('app.name', 'Laravel')); ?>
+                    <?php echo e(config('app.name', 'Bjora')); ?>
 
                 </a>
+                <span class="separator"></span>
+                <span id="time"></span>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="<?php echo e(__('Toggle navigation')); ?>">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -34,11 +53,16 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <?php if(auth()->guard()->check()): ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Ask Question</a>
+                            </li>
+                        <?php endif; ?>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+
                         <!-- Authentication Links -->
                         <?php if(auth()->guard()->guest()): ?>
                             <li class="nav-item">
@@ -56,6 +80,9 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Profile</a>
+                                    <a class="dropdown-item" href="#">My Questions</a>
+                                    <a class="dropdown-item" href="#">Inbox</a>
                                     <a class="dropdown-item" href="<?php echo e(route('logout')); ?>"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -78,6 +105,28 @@
             <?php echo $__env->yieldContent('content'); ?>
         </main>
     </div>
+    <script defer>
+        //show current time
+        (function showTime() {
+            let date = new Date();
+            let monthNames = [
+                "January", "February", "March",
+                "April", "May", "June", "July",
+                "August", "September", "October",
+                "November", "December"
+            ];
+            let day = date.getDate();
+            let monthIndex = date.getMonth();
+            let year = date.getFullYear();
+            let hour = date.getHours();
+            let minute = date.getMinutes();
+            let second = date.getSeconds();
+            document.getElementById('time').innerHTML = day + ' ' + monthNames[monthIndex] + ' ' +
+                year + ' - ' + ((hour < 10) ? '0'+hour : hour) + ':' + (minute < 10 ? '0'+minute : minute) + ':' + (second < 10 ? '0'+second : second);
+
+            setTimeout(showTime, 1000);
+        })();
+    </script>
 </body>
 </html>
 <?php /**PATH D:\Materi Kuliah\Semester 5\webProg\Project Akhir\final\resources\views/layouts/app.blade.php ENDPATH**/ ?>
