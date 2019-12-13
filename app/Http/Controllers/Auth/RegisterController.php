2536cff6e7b856b31ec2f306714632ad8c63d1e7
AzharM9 <56185093+AzharM9@users.noreply.github.com>
2019-12-13 13:09:44 +0700

    penyesuaian validasi register dg requirement di soal

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,9 +53,9 @@ class RegisterController extends Controller
             'dob.before' => 'Must be 18 years or older',
         ];
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:6', 'confirmed'],
+            'name' => ['required', 'string', 'max:100'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:6', 'confirmed', 'alpha_num'],
             'gender' => ['required', 'string'],
             'address' => ['required', 'string'],
             'dob' => ['required', 'date', 'before:-18 years'],
