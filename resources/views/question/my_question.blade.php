47430e5b30587a61092d4d553f30572aeb20d232
Kahfi Kusuma Negara <kahficlever@gmail.com>
2019-12-18 16:45:27 +0700

    add answer

--- resources/views/question/my_question.blade.php
+++ resources/views/question/my_question.blade.php
@@ -43,6 +43,64 @@
                                     <input type="hidden" name="id" value="{{$question->id}}">
                                     <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#remove-question">Delete</button>
 
+                                    <div class="row">
+                                            @foreach($answers as $answer)
+                                                <div class="col-md-12 mb-3">
+                                                    <div class="card">
+                                                        <div class="card-body">
+                                                            <div class="row pb-3">
+                                                                <div class="col-md-1">
+                                                                    <img src="{{ asset('files/'.$answer->user->picture) }}" width="36" height="36">
+                                                                </div>
+                                                                <div class="col-md-11">
+                                                                    <div class="row">
+                                                                        <div class="col-md-12">
+                                                                            {{ $answer->user->name }}
+                                                                        </div>
+                                                                        <div class="col-md-12">
+                                                                            <b>Answered at:</b> {{ $answer->created_at }}
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="col-md-12"><p>{{ $answer->answer }}</p></div>
+                                                                @if($answer->user_id == Auth::id())
+                                                                <div class="col-md-12">
+                                                                    <form method="post" action="{{ route('answer.destroy', [$answer->id]) }}">
+                                                                        @csrf
+                                                                        @method('DELETE')
+                                                                        <button type="submit" class="btn btn-link btn-sm">Delete Comment</button>
+                                                                    </form>
+                                                                </div>
+                                                                @else
+                                                                @endif
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            @endforeach
+                                            @if(!Auth::guest() && $question->status != 'closed')
+                                            <div class="col-md-12">
+                                                <form method="POST" action="{{ route('answer.store') }}">
+                                                    @csrf
+                                                    <input type="hidden" name="question_id" value="{{ $question->id }}">
+                                                    <div class="form-group row">
+                                                        <div class="col-md-12">
+                                                            <textarea placeholder="Answer" name="answer" rows="6" class="form-control"></textarea>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group row">
+                                                        <div class="col-md-12">
+                                                            <button type="submit" class="btn btn-primary">Answer</button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                            @endif
+                                        </div>
+
                                     <!-- Modal -->
                                     <div class="modal fade" id="remove-question" tabindex="-1" role="dialog" aria-labelledby="modalCenterTitle" aria-hidden="true">
                                         <div class="modal-dialog modal-dialog-centered" role="document">
